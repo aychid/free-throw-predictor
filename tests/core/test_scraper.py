60318f25a0e3a018a829ae8c7c81ab47f4d63bb1
aychid <aydinnajlhossaini@hotmail.com>
2024-10-31 19:43:09 +0100

    test: update test to scrape with no interval and try first 5 games

--- tests/core/test_scraper.py
+++ tests/core/test_scraper.py
@@ -3,7 +3,7 @@ import os
 
 
 class TestScraper:
-    url = "https://www.basketball-reference.com/players/j/jamesle01/gamelog/2018/"
+    URL = "https://www.basketball-reference.com/players/j/jamesle01/gamelog/2018/"
 
     def test_game_meta_data(self):
         """
@@ -12,8 +12,11 @@ class TestScraper:
         THEN the function should return a list of lists containing the date,
             home/away status, and opponent
         """
-        assert game_scraper.get_game_info(self.url)[0] == [[2017, 10, 17], 'Home', 'BOS'], \
-            "Test failed, game data not equal"
+        assert game_scraper.get_game_info(self.URL)[0] == [
+            [2017, 10, 17],
+            "Home",
+            "BOS",
+        ], "Test failed, game data not equal"
 
     def test_scrape_to_csv(self):
         """
@@ -22,10 +25,21 @@ class TestScraper:
         THEN the function should return 82 csv files containing the play
             by play data for each game in the pbp_games folder
         """
-        game_scraper.scrape_games(game_scraper.get_game_info(self.url))
-        csv_file_count = len([name for name in os.listdir('pbp_games')
-                             if os.path.isfile(os.path.join('pbp_games', name))
-                             and name.endswith('.csv')])
+        game_scraper.scrape_games(
+            game_scraper.get_game_info(self.URL), num_games=2, scrape_interval=0
+        )
+        csv_file_count = len(
+            [
+                name
+                for name in os.listdir("pbp_games")
+                if os.path.isfile(os.path.join("pbp_games", name))
+                and name.endswith(".csv")
+            ]
+        )
 
-        assert os.path.exists("pbp_games/2017_10_17_CLE_PBP_HOME.csv"), "Test failed to create first CSV file"
-        assert csv_file_count == 82, "Test failed, incorrect number of CSV files in folder"
+        assert os.path.exists(
+            "pbp_games/2017_10_17_CLE_PBP_HOME.csv"
+        ), "Test failed to create first CSV file"
+        assert (
+            csv_file_count == 82
+        ), "Test failed, incorrect number of CSV files in folder"
